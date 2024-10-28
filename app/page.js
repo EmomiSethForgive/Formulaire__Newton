5bee71b36632670cf10a05b0ca5ba96488796acd
TAMARA Forgive <ej613116@gmail.comgit config --global user.name TAMARA>
2024-10-28 10:15:47 +0100

    fini

--- app/page.js
+++ app/page.js
@@ -1,19 +1,19 @@
-import Image from "next/image";
-import { Forms } from "./components/Forms";
-import { Deuxième } from "./components/Deuxième";
-// import {RadioGroup} from "./components/RadioGroup";
+// import Image from "next/image";
+// import { Forms } from "./components/Forms";
+// import { Deuxième } from "./components/Deuxième";
+// // import {RadioGroup} from "./components/RadioGroup";
 
-export default function Home() {
-  return (
-    <div className=" w-full flex flex-col gap-4">
-      <h1 className="m-auto font-bold mt-3 md:text-xl">
-        FORMULAIRE DE SOUSCRIPTION
-      </h1>
-      <div className=" md:flex md:m-auto">
-        <Forms />
-        <Deuxième />
-        {/* <RadioGroup /> */}
-      </div>
-    </div>
-  );
-}
+// export default function Home() {
+//   return (
+//     <div className=" w-full flex flex-col gap-4">
+//       <h1 className="m-auto font-bold mt-3 md:text-xl">
+//         FORMULAIRE DE SOUSCRIPTION
+//       </h1>
+//       <div className=" md:flex md:m-auto">
+//         <Forms />
+//         <Deuxième />
+//         {/* <RadioGroup /> */}
+//       </div>
+//     </div>
+//   );
+// }
